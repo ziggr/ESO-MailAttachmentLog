21cd545aab0de1ac03d1bc46920796cbd4a5a2ca
Zig Zichterman <ziggr@ziggr.com>
2016-03-14 17:41:47 -0700

    MailRecord class to absorb a mail record

--- MailAttachmentLog.lua
+++ MailAttachmentLog.lua
@@ -6,6 +6,48 @@ MailAttachmentLog.default = {
       history = {}
 }
 
+-- MailRecord ----------------------------------------------------------------
+
+local MailRecord = {}
+
+function MailRecord:FromMailId(mail_id)
+    from_user,_, subject,_,_,_,_,_, attach_ct, attach_gold,
+         _,_, since_secs = GetMailItemInfo(mail_id)
+
+    o = { mail_id   = mail_id
+        , from      = from_user
+        , subject   = subject
+        , gold      = attach_gold
+        , attach_ct = attach_ct
+        , ts        = GetTimeStamp() - since_secs
+        }
+    setmetatable(o, self)
+    self.__index = self
+    o:LoadAttachments(mail_id)
+    return o
+end
+
+function MailRecord:LoadAttachments(mail_id)
+    for i = 1, self.attach_ct do
+        _,ct = GetAttachedItemInfo(mail_id, i)
+        link = GetAttachedItemLink(mail_id, i, LINK_STYLE_DEFAULT)
+        if not self.attach then
+            self.attach = {}
+        end
+        self.attach[i] = { ct   = ct
+                         , link = link
+                         }
+    end
+end
+
+function MailRecord:ToString()
+    return "from:"     ..tostring(self.from)
+        .." subject:"  ..tostring(self.subject)
+        .." attach_ct:"..tostring(self.attach_ct)
+end
+
+-- Init ----------------------------------------------------------------------
+
 function MailAttachmentLog.OnAddOnLoaded()
     if addonName ~= MailAttachmentLog.name then return end
     MailAttachmentLog:Initialize()
@@ -15,27 +57,30 @@ function MailAttachmentLog:Initialize()
     --
 end
 
+-- Do It ---------------------------------------------------------------------
+
 function MailAttachmentLog:DoIt()
     d(":DoIt")
 
     mail_id = GetNextMailId(nil)
     while mail_id do
-          senderDisplayName,
-          _,
-          subject,
-          _,
-          _,
-          _,
-          _,
-          _,
-          numbernumAttachments,
-          attachedMoney,
-          _,
-          _,
-          secsSinceReceived
-          = GetMailItemInfo(mail_id)
-          d(tostring(senderDisplayName).." "..tostring(subject).." att:"..tostring(numbernumAttachments))
-
+          -- senderDisplayName,
+          -- _,
+          -- subject,
+          -- _,
+          -- _,
+          -- _,
+          -- _,
+          -- _,
+          -- numbernumAttachments,
+          -- attachedMoney,
+          -- _,
+          -- _,
+          -- secsSinceReceived
+          -- = GetMailItemInfo(mail_id)
+          -- d(tostring(senderDisplayName).." "..tostring(subject).." att:"..tostring(numbernumAttachments))
+        mr = MailRecord:FromMailId(mail_id)
+        d(mr:ToString())
         mail_id = GetNextMailId(mail_id)
     end
 end
@@ -44,6 +89,8 @@ function MailAttachmentLog_DoIt()
     d("_DoIt")
     MailAttachmentLog:DoIt()
 end
+
+
 
 
 -- Postamble -----------------------------------------------------------------
